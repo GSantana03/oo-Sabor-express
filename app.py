148f690200850ea1aff1aec0c2ac15cb2cc98c39
Gustavo Santana <ribeiro99.gg@gmail.com>
2025-11-19 09:00:03 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gustavo', 10)
-restaurante_praca.receber_avaliacao('Verônica', 8)
-restaurante_praca.receber_avaliacao('Tayller', 2)
+bebida_suco = Bebida('Suco de Melância', 5.0,'grande')
+bebida_suco.aplicar_desconto()
+prato_paozinho = Prato('Paozinho',2.00,'o melhor pão da cidade')
+prato_paozinho.aplicar_desconto()
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 if __name__ == '__main__':
     main()
